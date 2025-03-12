7c698dacb6256fc853f246d25a28fd70089b72e5
GURUNANDINIDEVI <154117298+GURUNANDINIDEVI@users.noreply.github.com>
2025-03-12 22:12:32 +0530

    Create half_adder.v

---
+++ Combinational/half_adder.v
@@ -0,0 +1,11 @@
+module half_adder(
+    input wire A,    // Input A
+    input wire B,    // Input B
+    output wire Sum, // Sum output
+    output wire Carry // Carry output
+);
+
+assign Sum = A ^ B;    // XOR for sum
+assign Carry = A & B;  // AND for carry
+
+endmodule
