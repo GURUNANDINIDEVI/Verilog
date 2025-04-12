e12ffbdf1d04c12b4be535900b7d18de1a6d7b84
GURUNANDINIDEVI <154117298+GURUNANDINIDEVI@users.noreply.github.com>
2025-04-12 20:36:36 +0530

    2:4_decoder_design.v

---
+++ Day - 11/2:4_decoder_design.v
@@ -0,0 +1,11 @@
+
+//T.Guru Nandini Devi
+//nandinidevitekumudi@gmail.com
+module decoder_2to4 (
+    input wire [1:0] in,     // 2-bit input
+    input wire en,           // Enable
+    output wire [3:0] out    // 4-bit output
+);
+assign out = (en == 1'b1) ? (4'b0001 << in) : 4'b0000;
+
+endmodule
