11ab9a9c774f56f28fb0218f116de2a0f8f9f511
GURUNANDINIDEVI <154117298+GURUNANDINIDEVI@users.noreply.github.com>
2025-04-12 19:18:46 +0530

    Create or_gate_tb.v

---
+++ basics/Day - 01/or_gate_tb.v
@@ -0,0 +1,24 @@
+module or_gate_tb;
+    reg a; // Inputs
+    reg b;
+    wire y;//outputs
+ // Instantiate the OR gate
+    or_gate uut (
+        .a(a),
+        .b(b),
+        .y(y)
+    );
+    // Test stimulus
+    initial begin
+        $display("Time\t a b | y");
+        $monitor("%g\t %b %b | %b", $time, a, b, y);
+
+        // Test all combinations
+        a = 0; b = 0; #10;
+        a = 0; b = 1; #10;
+        a = 1; b = 0; #10;
+        a = 1; b = 1; #10;
+
+        $finish;
+    end
+endmodule
