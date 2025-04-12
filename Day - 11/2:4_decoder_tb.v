5abc94b373ff9f5773e352b079cb7a5e60c8c371
GURUNANDINIDEVI <154117298+GURUNANDINIDEVI@users.noreply.github.com>
2025-04-12 21:06:02 +0530

    Update 2:4_decoder_tb.v

--- Day - 11/2:4_decoder_tb.v
+++ Day - 11/2:4_decoder_tb.v
@@ -1,26 +1,21 @@
 //T.Guru Nandini Devi
 //nandinidevitekumudi@gmail.com
 module tb_decoder_2to4;
-
     reg [1:0] in;
     reg en;
     wire [3:0] out;
-
     // Instantiate the Decoder
-    decoder_2to4 uut (
+    decoder_2to4 u1 (
         .in(in),
         .en(en),
         .out(out)
     );
-
     initial begin
         $display("Time\t en in | out");
         $monitor("%g\t %b  %b  | %b", $time, en, in, out);
-
         // Test with enable OFF
         en = 0; in = 2'b00; #10;
         in = 2'b01; #10;
-
         // Test with enable ON
         en = 1; in = 2'b00; #10;
         in = 2'b01; #10;
