a04935553d55721f085d57a5ad136789f5a1fae5
GURUNANDINIDEVI <154117298+GURUNANDINIDEVI@users.noreply.github.com>
2025-04-12 19:54:14 +0530

    Create half_sub_tb.v

---
+++ Day - 04/half_sub_tb.v
@@ -0,0 +1,29 @@
+module half_subtractor_tb;
+    // Inputs
+    reg a;
+    reg b;
+   // Outputs
+    wire difference;
+    wire borrow;
+
+    // Instantiate the Half Subtractor
+    half_subtractor u1 (
+        .a(a),
+        .b(b),
+      .difference(difference),
+        .borrow(borrow)
+    );
+
+    // Test stimulus
+    initial begin
+      $display("Time\t a b | difference borrow");
+      $monitor("%g\t %b %b |   %b     %b", $time, a, b, difference, borrow);
+
+        a = 0; b = 0; #10;
+        a = 0; b = 1; #10;
+        a = 1; b = 0; #10;
+        a = 1; b = 1; #10;
+
+        $finish;
+    end
+endmodule
