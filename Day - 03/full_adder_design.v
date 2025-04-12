fa9a4fc899af17fb29f41267af1b2dda62aec714
GURUNANDINIDEVI <154117298+GURUNANDINIDEVI@users.noreply.github.com>
2025-04-12 20:59:41 +0530

    Update full_adder_design.v

--- Day - 03/full_adder_design.v
+++ Day - 03/full_adder_design.v
@@ -1,14 +1,14 @@
 //T.Guru Nandini Devi
 //nandinidevitekumudi@gmail.com
 module full_adder(
-    input wire A,       // Input A
-    input wire B,       // Input B
-    input wire Cin,     // Input Carry (Cin)
-    output wire Sum,    // Sum output
-    output wire Cout    // Carry output (Cout)
+    input wire a,       // Input a
+    input wire b,       // Input b
+    input wire cin,     // Input carry (cin)
+    output wire sum,    // sum output
+    output wire cout    // carry output (cout)
 );
 
-assign Sum = A ^ B ^ Cin;      // Sum is the XOR of A, B, and Cin
-assign Cout = (A & B) | (Cin & (A ^ B)); // Carry is the OR of (A AND B) and (Cin AND (A XOR B))
+assign Sum = a ^ b ^ cin;      // sum is the XOR of a, b, and cin
+    assign Cout = (a & b) | (cin & (a ^ b)); // carry is the OR of (a AND b) and (cin AND (a XOR b))
 
 endmodule
