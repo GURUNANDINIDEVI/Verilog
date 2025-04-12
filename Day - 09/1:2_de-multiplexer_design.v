cedb0d51585c15b2f4af56cdc3bbe3c2cbe09e54
GURUNANDINIDEVI <154117298+GURUNANDINIDEVI@users.noreply.github.com>
2025-04-12 20:26:27 +0530

    1:2_de-multiplexer_design.v

---
+++ Day - 09/1:2_de-multiplexer_design.v
@@ -0,0 +1,12 @@
+module demux_1to2 (
+    input wire din,        // Data input
+    input wire sel,        // Select line
+    output wire y0,        // Output 0
+    output wire y1         // Output 1
+);
+
+assign y0 = (~sel) & din;
+assign y1 = sel & din;
+
+endmodule
+
