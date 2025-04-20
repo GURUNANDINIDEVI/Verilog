f28e1728b383fb1489e4dea7fd2c0aebc40f9913
GURUNANDINIDEVI <154117298+GURUNANDINIDEVI@users.noreply.github.com>
2025-04-20 18:50:48 +0530

    .

---
+++ Day - 18/2-bit Binary Multiplier_design.v
@@ -0,0 +1,9 @@
+//T.Guru Nandini Devi
+//nandinidevitekumudigmail.com
+module binary_multiplier_2bit (
+    input  [1:0] A,
+    input  [1:0] B,
+    output [3:0] Product
+);
+    assign Product = A * B;
+endmodule
