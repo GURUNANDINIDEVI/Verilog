2682b0f2932db7b0c4e92f9aa9c4ae9bb53f8d26
GURUNANDINIDEVI <154117298+GURUNANDINIDEVI@users.noreply.github.com>
2025-05-19 16:51:43 +0530

    T_latch_design.v

--- Day - 33/T_latch_design.v
+++ Day - 33/T_latch_design.v
@@ -5,7 +5,7 @@ module t_latch (
     input clk,   // Clock input (used as enable here)
     output reg Q // Output
 );
-
+initial Q = 0;  // Initialize Q to 0 at simulation start
 always @(clk or T) begin
     if (clk) begin
         if (T)
