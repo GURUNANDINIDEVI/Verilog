79bc7313ef389eeda0c624ae97bc083bc26b0dce
GURUNANDINIDEVI <154117298+GURUNANDINIDEVI@users.noreply.github.com>
2025-05-19 16:30:19 +0530

    JK Latch_tb.v

--- Day - 32/JK Latch_tb.v
+++ Day - 32/JK Latch_tb.v
@@ -13,8 +13,8 @@ module tb_jk_latch;
 
     // Stimulus block
     initial begin
-        $display("Time\tj\tk\ten\trst\tq");
-        $monitor("%0dns\t%b\t%b\t%b\t%b\t%b", $time, j,k, en, rst, q);
+        $display("Time\tJ\tK\tenable\treset\tQ");
+      $monitor("%0dns\t%b\t%b\t%b\t%b\t%b", $time, J,K, enable, reset, Q);
         // Initialize
         J = 0; K = 0; enable = 0; reset = 1;
 
