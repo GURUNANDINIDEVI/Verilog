5c53ed29b404651e1f67497cbe7a68c0c8446587
Tekumudi Guru Nandini Devi <154117298+GURUNANDINIDEVI@users.noreply.github.com>
2025-06-17 22:23:42 +0530

    Ring Counter

--- Day - 40/Ring Counter tb.v
+++ Day - 40/Ring Counter tb.v
@@ -1,27 +1,46 @@
-//T.Guru Nandini Devi
-//nandinidevitekumudi@gmail.com
+// T.Guru Nandini Devi
+// nandinidevitekumudi@gmail.com
+
+// Testbench for Ring Counter
 module tb_ring_counter;
-reg clk;
-reg rst;
-wire [3:0] out;
 
-ring_counter u1(.clk(clk),.rst(rst),.out(out));
+    // Declare signals
+    reg clk;
+    reg rst;
+    wire [3:0] out;
 
-// Clock generation (10ns period)
-always #5 clk = ~clk;
+    // Instantiate Ring Counter
+    ring_counter u1(
+        .clk(clk),
+        .rst(rst),
+        .out(out)
+    );
 
-initial begin
-    $dumpfile("ring_counter.vcd");
-    $dumpvars(0, tb_ring_counter);
+    // Clock generation with 10ns period
+    always #5 clk = ~clk;
 
-    // Initialization
-    clk = 0; rst = 1;
+    // Monitor signals at each clock pulse
+    always @(posedge clk) begin
+        $display("Time = %t | reset = %b | out = %b",$time, rst, out);
+    end
 
-    // Apply reset
-    #10 rst = 0;
+    initial begin
+        // Dump signals for waveform view
+        $dumpfile("ring_counter.vcd");
 
-    // Let it run for 8 cycles
-    #80 $finish;
-end
+        $dumpvars(0, tb_ring_counter);
+
+        // Initial reset
+        clk = 0;
+        rst = 1;
+
+        // Release reset after 10 ns
+        #10
+        rst = 0;
+
+        // Let it run for 8 clock cycles
+        #80
+        $finish;
+    end
 
 endmodule
