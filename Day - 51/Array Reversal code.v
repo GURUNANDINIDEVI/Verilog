302b213fffddad4b0e62fc4ccd27ad83b8764809
Tekumudi Guru Nandini Devi <154117298+GURUNANDINIDEVI@users.noreply.github.com>
2025-06-23 20:47:21 +0530

    Array Reversal

--- Day - 51/Array Reversal code.v
+++ Day - 51/Array Reversal code.v
@@ -4,15 +4,15 @@ module reverse_array #(
     parameter N = 5,
     parameter WIDTH = 8
 )(
-    input  [WIDTH-1:0] in_array [0:N-1],
-    output [WIDTH-1:0] out_array [0:N-1]
+    input  wire [N*WIDTH-1:0] in_array_flat,
+    output reg  [N*WIDTH-1:0] out_array_flat
 );
 
     integer i;
 
     always @(*) begin
         for (i = 0; i < N; i = i + 1) begin
-            out_array[i] = in_array[N-1 - i];
+            out_array_flat[i*WIDTH +: WIDTH] = in_array_flat[(N-1-i)*WIDTH +: WIDTH];
         end
     end
 
