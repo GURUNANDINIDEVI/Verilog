13b85d4ca64730063526872a4911c3c519037fcf
Tekumudi Guru Nandini Devi <154117298+GURUNANDINIDEVI@users.noreply.github.com>
2025-06-23 20:48:01 +0530

    Array Reversal

--- Day - 51/Array Reversal tb.v
+++ Day - 51/Array Reversal tb.v
@@ -6,15 +6,34 @@ module tb_reverse_array;
     parameter N = 5;
     parameter WIDTH = 8;
 
-    reg [WIDTH-1:0] in_array [0:N-1];
-    wire [WIDTH-1:0] out_array [0:N-1];
+    reg  [WIDTH-1:0] in_array     [0:N-1];
+    wire [WIDTH-1:0] out_array    [0:N-1];
+    reg  [N*WIDTH-1:0] in_array_flat;
+    wire [N*WIDTH-1:0] out_array_flat;
+
     integer i;
 
-    // Instantiating the reverse array
-  reverse_array #(N, WIDTH) u1 (.in_array(in_array),.out_array(out_array));
+    // Flattening input array
+    always @(*) begin
+        for (i = 0; i < N; i = i + 1)
+            in_array_flat[i*WIDTH +: WIDTH] = in_array[i];
+    end
+
+    // Unflattening output array
+    genvar j;
+    generate
+        for (j = 0; j < N; j = j + 1)
+            assign out_array[j] = out_array_flat[j*WIDTH +: WIDTH];
+    endgenerate
+
+    // Instantiating the reverse_array module
+    reverse_array #(N, WIDTH) u1 (
+        .in_array_flat(in_array_flat),
+        .out_array_flat(out_array_flat)
+    );
 
     initial begin
-        // Initializing the array
+        // Initializing the input array
         in_array[0] = 8'd1;
         in_array[1] = 8'd2;
         in_array[2] = 8'd3;
@@ -24,14 +43,12 @@ module tb_reverse_array;
         #5;
 
         $display("Original array:");
-        for (i = 0; i < N; i = i + 1) begin
-            $display("%d : %d", i, in_array[i]);
-        end
+        for (i = 0; i < N; i = i + 1)
+            $display("Index %0d : %0d", i, in_array[i]);
 
         $display("\nReversed array:");
-        for (i = 0; i < N; i = i + 1) begin
-            $display("%d : %d", i, out_array[i]);
-        end
+        for (i = 0; i < N; i = i + 1)
+            $display("Index %0d : %0d", i, out_array[i]);
 
         $finish;
     end
