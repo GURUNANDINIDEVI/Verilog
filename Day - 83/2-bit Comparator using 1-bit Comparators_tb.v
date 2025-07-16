be327720862e0f5618adee11c2bd1ba3b2d87c38
Tekumudi Guru Nandini Devi <154117298+GURUNANDINIDEVI@users.noreply.github.com>
2025-07-16 12:07:19 +0530

    2-bit Comparator using 1-bit Comparators

---
+++ Day - 83/2-bit Comparator using 1-bit Comparators_tb.v
@@ -0,0 +1,22 @@
+//T.Guru Nandini devi
+//nandinidevitekumudi@gmail.com
+module tb_comparator_2bit;
+    reg [1:0] a, b;
+    wire gt, eq, lt;
+
+    comparator_2bit u1 (
+        .a(a), .b(b),
+        .a_gt_b(gt), .a_eq_b(eq), .a_lt_b(lt)
+    );
+
+    initial begin
+        $display("A  B  | GT EQ LT");
+        for (integer i = 0; i < 4; i = i + 1) begin
+            for (integer j = 0; j < 4; j = j + 1) begin
+                a = i[1:0]; b = j[1:0]; #5;
+                $display("%b %b |  %b  %b  %b", a, b, gt, eq, lt);
+            end
+        end
+        $finish;
+    end
+endmodule
