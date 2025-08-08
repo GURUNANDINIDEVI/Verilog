de82c7f1af7aa04922dfc9defebe7ae464c158d6
Tekumudi Guru Nandini Devi <154117298+GURUNANDINIDEVI@users.noreply.github.com>
2025-08-08 21:49:38 +0530

    D_FF_using_SR_FF

--- Day - 94/D_FF_using_SR_FF.v
+++ Day - 94/D_FF_using_SR_FF.v
@@ -1,30 +1,31 @@
 //T.Guru Nandini Devi
 //nandinidevitekumudi@gmail.com
-module sr_ff (
-    input clk, S, R,
+module sr_flipflop (
+    input S, R, clk,
     output reg Q
 );
     always @(posedge clk) begin
-        if (S == 1 && R == 0)
-            Q <= 1;
-        else if (S == 0 && R == 1)
-            Q <= 0;
-        else if (S == 0 && R == 0)
-            Q <= Q;  // Hold
-        else
-            Q <= 1'bx; // Invalid condition (S=1, R=1)
+        case ({S, R})
+            2'b00: Q <= Q;       // No change
+            2'b01: Q <= 0;       // Reset
+            2'b10: Q <= 1;       // Set
+            2'b11: Q <= 1'bx;    // Invalid
+        endcase
     end
 endmodule
 
-module d_ff_using_sr (
-    input clk, D,
+module d_flipflop_using_sr (
+    input D, clk,
     output Q
 );
     wire S, R;
-
     assign S = D;
     assign R = ~D;
 
-    sr_ff sr(.clk(clk), .S(S), .R(R), .Q(Q));
+    sr_flipflop sr1 (
+        .S(S),
+        .R(R),
+        .clk(clk),
+        .Q(Q)
+    );
 endmodule
-
