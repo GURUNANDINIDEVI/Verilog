3cf1aa893ab3776633f253b6ae0d0df1d767b525
Tekumudi Guru Nandini Devi <154117298+GURUNANDINIDEVI@users.noreply.github.com>
2025-08-10 09:30:21 +0530

    SR_FF using D_FF

--- Day - 95/SR_FF using D_FF.v
+++ Day - 95/SR_FF using D_FF.v
@@ -1,20 +1,25 @@
 //T.Guru Nandini Devi
 //nandinidevitekumudi@gmail.com
-module d_ff (
-    input clk, D,
+module d_flipflop (
+    input D, clk,
     output reg Q
 );
     always @(posedge clk)
         Q <= D;
 endmodule
 
-module sr_using_dff (
-    input clk, S, R,
+module sr_using_d (
+    input S, R, clk,
     output Q
 );
     wire D;
+    reg Q_int;  // Internal storage for current state
 
-    assign D = (S | (~R & Q)); // Logic to convert S, R into D
+    // D equation: S + (~R & Q)
+    assign D = S | (~R & Q_int);
 
-    d_ff d1 (.clk(clk), .D(D), .Q(Q));
+    // Instantiate D FF
+    d_flipflop dff1 (.D(D), .clk(clk), .Q(Q_int));
+
+    assign Q = Q_int;
 endmodule
