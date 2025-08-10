931d0368e8d98ec26350b26be4096ef9139e51ac
Tekumudi Guru Nandini Devi <154117298+GURUNANDINIDEVI@users.noreply.github.com>
2025-08-10 09:51:14 +0530

    T to SR Flip-Flop

--- Day - 96/T to SR Flip-Flop.v
+++ Day - 96/T to SR Flip-Flop.v
@@ -1,11 +1,13 @@
 //T.Guru Nandini Devi
 //nandinidevitekumudi@gmail.com
 module sr_ff (
-    input clk, S, R,
+    input S, R, clk,
     output reg Q
 );
+    initial Q = 0; // start with known value
+
     always @(posedge clk) begin
-        case ({S, R})
+        case ({S,R})
             2'b00: Q <= Q;     // Hold
             2'b01: Q <= 0;     // Reset
             2'b10: Q <= 1;     // Set
@@ -15,13 +17,16 @@ module sr_ff (
 endmodule
 
 module t_using_sr (
-    input clk, T,
+    input T, clk,
     output Q
 );
-    wire S, R;
+    wire S, R, Q_int; // Q_int is wire now
 
-    assign S = T & ~Q;
-    assign R = T & Q;
+    assign S = T & ~Q_int;
+    assign R = T &  Q_int;
 
-    sr_ff sr1 (.clk(clk), .S(S), .R(R), .Q(Q));
+    // SR FF instance
+    sr_ff sr1 (.S(S), .R(R), .clk(clk), .Q(Q_int));
+
+    assign Q = Q_int; // Output mapping
 endmodule
